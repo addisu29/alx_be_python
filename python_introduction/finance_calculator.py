dbe3671df47a80ad91fd84b2c3e7c010154a8024
Addisu Merga <addisum2129@gmail.com>
2025-11-06 01:03:07 -0800

    fix: match checker format for monthly_savings calculation

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,12 +1,12 @@
 
 
 
-income = float(input("Enter your monthly income: "))
-expenses = float(input("Enter your monthly expenses: "))
+monthly_income = float(input("Enter your monthly income: "))
+monthly_expenses = float(input("Enter your monthly expenses: "))
 
 
 
-monthly_savings = income-expenses
+monthly_savings = monthly_income - monthly_expenses
 
 
 projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
